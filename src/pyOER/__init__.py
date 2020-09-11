97066cbd09c823b7fa0164cf5dc3029e1bba8178
Jakob Ejler Sørensen <lillebror.jakobes@gmail.com>
2020-09-11 22:54:55 +0200

    Helpful python version check >= 3.6

--- src/pyOER/__init__.py
+++ src/pyOER/__init__.py
@@ -1,7 +1,15 @@
 __version__ = "0.0.1"
 
-print(f"importing pyOER v{__version__} from {__file__}")
+# Module utilizes internal functions only available from V3.6+
+import sys
+major, minor = sys.version_info.major, sys.version_info.minor
+if major < 3 or (major > 2 and minor < 6):
+    raise SystemError('This module requires python3.6 or newer. You are using {}.{}'.format(major, minor))
+else:
+    print('Python version check: ok')
 
+# Use old formatting syntax to prevent a SyntaxError from running above version check
+print("importing pyOER v{} from {}".format(__version__, __file__))
 
 from .measurement import Measurement, MeasurementCounter, all_measurements
 from .calibration import Calibration, CalibrationCounter, all_calibrations
