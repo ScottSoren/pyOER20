0a4897e3770b73bd32a74c5210377ce2cf95cac9
soren@thomson <scott.soren@gmail.com>
2021-07-26 17:02:30 +0200

    final figure: 3x norm flux

--- figures/paper_II_v3_figs/paper_II_v3_fig5.py
+++ figures/paper_II_v3_figs/paper_II_v3_fig5.py
@@ -1,5 +1,12 @@
+import json
+import numpy as np
 from matplotlib import pyplot as plt
 from pyOER import StabilityResultsCollection
+from pyOER.constants import (
+    FARADAY_CONSTANT,
+    STANDARD_SPECIFIC_CAPACITANCE,
+    STANDARD_SITE_DENSITY,
+)
 
 forpublication = True
 if forpublication:  # for the publication figure
@@ -13,43 +20,112 @@ if forpublication:  # for the publication figure
 else:
     plt.style.use("default")
 
-
 results_collection = StabilityResultsCollection.open("results_collection.json")
 
+fig5_sample_type = "RT-RuO2"
+fig5_sample_name = "Reshma1"
 
-if True:  # fig 3a
-    fig3a, ax3a = plt.subplots()
-    fig3a_sample_type = "RT-RuO2"
+if True:  # fig 5a, with stability numbers plotted against total current density.
+    fig5a, ax5a = plt.subplots()
     current_positions = {"0.05": 1, "0.15": 2, "0.5": 3, "1.0": 4}
     number_specs = {
         "S_number": {"marker": "^", "color": "k"},
         "S_number_lattice": {"marker": "v", "color": "#54bdebff"},
     }
     for current_point, position in current_positions.items():
-        stats = results_collection.get_stats(fig3a_sample_type, current_point, "steady")
+        stats = results_collection.get_stats(fig5_sample_type, current_point, "steady")
         for number_name, specs in number_specs.items():
             number, sigma_number = stats[number_name]
-            ax3a.plot(position, number, **specs)
+            ax5a.plot(position, number, **specs)
             if sigma_number:
                 error_specs = specs.copy()
                 error_specs.update(marker="_")
-                ax3a.plot(
+                ax5a.plot(
                     [position, position],
                     [number - sigma_number, number + sigma_number],
                     **error_specs,
                 )
-    ax3a.set_yscale("log")
+
+    ax5a.set_yscale("log")
 
     ticklabels = ["0.05", "0.15", "0.5", "1.0"]
-    ax3a.set_xticks([current_positions[c] for c in ticklabels])
-    ax3a.set_xticklabels(ticklabels)
-    ax3a.set_ylabel("stability number")
-    ax3a.set_xlabel("current density / (mA cm$^{-2}_{geo}$)")
-    ax_r = ax3a.twinx()
+    ax5a.set_xticks([current_positions[c] for c in ticklabels])
+    ax5a.set_xticklabels(ticklabels)
+    ax5a.set_ylabel("stability number")
+    ax5a.set_xlabel("current density / (mA cm$^{-2}_{geo}$)")
+    ax_r = ax5a.twinx()
     ax_r.set_yscale("log")
-    ax_r.set_ylim([1 / y for y in ax3a.get_ylim()])
+    ax_r.set_ylim([1 / y for y in ax5a.get_ylim()])
     ax_r.set_ylabel("atoms per O$_2$")
 
     if forpublication:
-        fig3a.savefig("paper_II_fig_3a.png")
-        fig3a.savefig("paper_II_fig_3a.svg")
+        fig5a.savefig("paper_II_v3_fig5a.png")
+        fig5a.savefig("paper_II_v3_fig5a.svg")
+
+if True:  # fig 5b, with norm. activity, exchange, and diss. plotted against potential!
+    import sys
+    from pathlib import Path
+
+    paper_I_figs_path = Path("../paper_I_v5_figs").absolute().resolve()
+    sys.path.append(str(paper_I_figs_path))
+    from paper_I_v5_fig3 import plot_all_activity_results
+    from paper_I_v5_fig5 import get_model_j_norm
+
+    if False:  # calculate mean capacitance of Reshma1 samples.
+        cap_exp_list = [
+            exp
+            for exp in all_activity_experiments()
+            if fig5_sample_name in exp.sample_name
+        ]
+        cap = np.mean([exp.cap for exp in cap_exp_list])
+    else:
+        cap = 0.0017  # mean capacitance of Reshma1 samples in [F]
+
+    with open("../paper_I_v5_figs/fit_results.json") as f:
+        fit_results = json.load(f)
+    u_model = np.linspace(1.28, 1.5, 100)
+    j_model = get_model_j_norm(u=u_model, **fit_results)  # [A/F
+    n_dot_model = j_model / (4 * FARADAY_CONSTANT) * 1e9  # [nmol/s/F]
+
+    fig5b, ax5b = plt.subplots()
+    # a factor to convert from TOF in [s^-1] to norm flux in [nmol/s/F]
+    factor = STANDARD_SITE_DENSITY / STANDARD_SPECIFIC_CAPACITANCE * 1e9
+    plt.rc("lines", markersize=3)
+    plot_all_activity_results(ax=ax5b, for_model=True, result="tof", factor=factor)
+    plt.rc("lines", markersize=6)
+    ax5b.plot(u_model, n_dot_model, "k--")
+
+    current_point_mapping = {
+        s: float(s) for s in ["1.0", "0.5", "0.25", "0.15", "0.05"]
+    }
+    for current_point, j_mA_cm2 in current_point_mapping.items():
+        j_norm = j_mA_cm2 * 0.196 * 1e-3 / cap  # [mA/cm^2] --> [A/F]
+        n_dot_norm = j_norm / (4 * FARADAY_CONSTANT) * 1e9  # [nmol/s/F]
+        u = np.interp(n_dot_norm, n_dot_model, u_model)
+        stats = results_collection.get_stats(fig5_sample_type, current_point, "steady")
+        for number_name, specs in number_specs.items():
+            number, sigma_number = stats[number_name]
+            ax5b.plot(u, n_dot_norm / number, **specs)
+            if sigma_number:
+                error_specs = specs.copy()
+                error_specs.update(marker="_")
+                ax5b.plot(
+                    [u, u],
+                    [
+                        n_dot_norm / (number - sigma_number),
+                        n_dot_norm / (number + sigma_number),
+                    ],
+                    **error_specs,
+                )
+
+    ax5b.set_yscale("log")
+    ax5b.set_xlabel("E vs RHE / (V)")
+    ax5b.set_ylabel("norm. rate / (pmol s$^{-1}$ F$^{-1}$)")
+    ax_r = ax5b.twinx()
+    ax_r.set_ylim([l / factor for l in ax5b.get_ylim()])
+    ax_r.set_yscale("log")
+    ax_r.set_ylabel("turn-over frequency / (s$^{-1}$)")
+
+    if forpublication:
+        fig5b.savefig("paper_II_v3_fig5b.png")
+        fig5b.savefig("paper_II_v3_fig5b.svg")
--- figures/paper_I_v5_figs/paper_I_v5_fig5.py
+++ figures/paper_I_v5_figs/paper_I_v5_fig5.py
@@ -1,5 +1,6 @@
 """This module plots stuff from 21F10 in Spectro Inlets notebook."""
 
+import json
 import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib import gridspec
@@ -16,21 +17,6 @@ from pyOER.constants import (
 )
 from paper_I_v5_fig3 import plot_all_activity_results
 from pyOER.modelling import get_states
-
-forpublication = True
-# plt.interactive(False)  # show the plot when I tell you to show() it!
-
-if forpublication:  # for the publication figure
-    import matplotlib as mpl
-
-    mpl.rcParams["figure.figsize"] = (3.25, 2.75)
-    # plt.rc('text', usetex=True)  # crashingly slow
-    plt.rc("font", family="sans-serif")
-    plt.rc("font", size=8)
-    plt.rc("lines", linewidth=0.5)
-    plt.rc("lines", markersize=3)
-else:
-    plt.style.use("default")
 
 
 try:  # See if the results to fit have already been gathered and saved
@@ -110,110 +96,128 @@ def square_error(params):
     return error.dot(error)
 
 
-result = minimize(
-    square_error,
-    np.array([-0.1, -0.3, 2e-3]),
-    bounds=[(None, None), (None, None), (1e-10, None)],
-)
+if __name__ == "__main__":
 
-G1, G2, j_norm_0 = result.x
+    forpublication = True
+    # plt.interactive(False)  # show the plot when I tell you to show() it!
 
-u = np.linspace(1.23, 1.55, 100)
-j_over_j0 = get_model_j_over_j0(G1, G2, u)
+    if forpublication:  # for the publication figure
+        import matplotlib as mpl
 
-gs = gridspec.GridSpec(8, 1)
-# gs.update(hspace=0.025)
-ax1 = plt.subplot(gs[0:4, 0])
-ax2 = plt.subplot(gs[4:6, 0])
-ax3 = ax2.twinx()
-ax4 = plt.subplot(gs[6:8, 0])
+        mpl.rcParams["figure.figsize"] = (3.25, 2.75)
+        # plt.rc('text', usetex=True)  # crashingly slow
+        plt.rc("font", family="sans-serif")
+        plt.rc("font", size=8)
+        plt.rc("lines", linewidth=0.5)
+        plt.rc("lines", markersize=3)
+    else:
+        plt.style.use("default")
 
-ax1.xaxis.set_label_position("top")
-ax1.tick_params(axis="x", top=True, bottom=True, labeltop=True, labelbottom=False)
-ax2.tick_params(axis="x", top=False, bottom=True, labeltop=False, labelbottom=False)
-ax4.tick_params(axis="x", top=False, bottom=True, labeltop=False, labelbottom=True)
-ax3.spines["right"].set_color("r")
-ax3.tick_params(axis="y", color="r")
-ax3.tick_params(axis="y", labelcolor="r")
-ax3.yaxis.label.set_color("r")
-
-fig = ax1.get_figure()
-
-
-if True:  # plot the experimental results with the colors (a bit slow):
-    tof_0 = j_norm_0 / STANDARD_SITE_DENSITY * STANDARD_SPECIFIC_CAPACITANCE / (4 * F)
-    plot_all_activity_results(ax=ax1, result="tof", factor=1 / tof_0, takelog=True)
-elif True:  # plot all the experimental results black and white:
-    ax1.plot(
-        u_exp,
-        np.log10(j_norm_exp / j_norm_0),
-        "k",
-        marker="o",
-        linestyle="none",
-        fillstyle="none",
+    result = minimize(
+        square_error,
+        np.array([-0.1, -0.3, 2e-3]),
+        bounds=[(None, None), (None, None), (1e-10, None)],
     )
 
-ax1.plot(u, np.log10(j_over_j0), linewidth=0.6, color="k")
+    G1, G2, j_norm_0 = result.x
 
-ax1.set_xlabel("U$_{RHE}$ / [V]")
-ax1.set_ylabel("log(j / j$_{RDS}^0$)")
+    with open("fit_results.json", "w") as f:
+        json.dump({"G1": G1, "G2": G2, "j_norm_0": j_norm_0}, f)
 
+    u = np.linspace(1.23, 1.55, 100)
+    j_over_j0 = get_model_j_over_j0(G1, G2, u)
 
-if True:  # explanatory fig
+    gs = gridspec.GridSpec(8, 1)
+    # gs.update(hspace=0.025)
+    ax1 = plt.subplot(gs[0:4, 0])
+    ax2 = plt.subplot(gs[4:6, 0])
+    ax3 = ax2.twinx()
+    ax4 = plt.subplot(gs[6:8, 0])
 
-    states = get_states(G1, G2)
-    theta_over_theta_i = np.array(
-        [
-            1 / state.K_rds * np.exp(-state.n_to_rds * F * (u - U0) / (R * T0))
-            for state in states
-        ]
-    )
-    # ... and this matrix is the coverage of each individual state:
-    thetas = theta_over_theta_i / np.sum(theta_over_theta_i, axis=0)
+    ax1.xaxis.set_label_position("top")
+    ax1.tick_params(axis="x", top=True, bottom=True, labeltop=True, labelbottom=False)
+    ax2.tick_params(axis="x", top=False, bottom=True, labeltop=False, labelbottom=False)
+    ax4.tick_params(axis="x", top=False, bottom=True, labeltop=False, labelbottom=True)
+    ax3.spines["right"].set_color("r")
+    ax3.tick_params(axis="y", color="r")
+    ax3.tick_params(axis="y", labelcolor="r")
+    ax3.yaxis.label.set_color("r")
 
-    # ...and also the average number of electron transfers to get to the RDS:
-    n_to_rds_vec = np.sum(
-        [thetas[j] * state.n_to_rds for j, state in enumerate(states)], axis=0
-    )
+    fig = ax1.get_figure()
 
-    for j, state in enumerate(states):
-        ax2.plot(u, thetas[j], state.color)
-    ax3.plot(u, n_to_rds_vec, "r--")
+    if True:  # plot the experimental results with the colors (a bit slow):
+        tof_0 = (
+            j_norm_0 / STANDARD_SITE_DENSITY * STANDARD_SPECIFIC_CAPACITANCE / (4 * F)
+        )
+        plot_all_activity_results(ax=ax1, result="tof", factor=1 / tof_0, takelog=True)
+    elif True:  # plot all the experimental results black and white:
+        ax1.plot(
+            u_exp,
+            np.log10(j_norm_exp / j_norm_0),
+            "k",
+            marker="o",
+            linestyle="none",
+            fillstyle="none",
+        )
 
-    # ax2.set_xlabel("U$_{RHE}$ / [V]")
-    ax2.set_ylabel("coverage")
-    ax3.set_ylabel("<n> before RDS")
+    ax1.plot(u, np.log10(j_over_j0), linewidth=0.6, color="k")
 
-if True:
-    # delta G plot
-    u_vec = np.array([1.23, 1.55])
-    for state in states:
-        n = state.n_to_rds
-        G0 = state.eV_1p23_vs_rds
-        G_vec = G0 + (u_vec - U0) * n
-        ax4.plot(u_vec, G_vec, state.color)
+    ax1.set_xlabel("U$_{RHE}$ / [V]")
+    ax1.set_ylabel("log(j / j$_{RDS}^0$)")
 
-    ax4.set_ylim(top=0.1)
+    if True:  # explanatory fig
 
-    ax4.set_xlabel("U vs RHE / [V]")
-    ax4.set_ylabel("$G^0 - G^0_{RDS}$ / [eV]")
+        states = get_states(G1, G2)
+        theta_over_theta_i = np.array(
+            [
+                1 / state.K_rds * np.exp(-state.n_to_rds * F * (u - U0) / (R * T0))
+                for state in states
+            ]
+        )
+        # ... and this matrix is the coverage of each individual state:
+        thetas = theta_over_theta_i / np.sum(theta_over_theta_i, axis=0)
 
+        # ...and also the average number of electron transfers to get to the RDS:
+        n_to_rds_vec = np.sum(
+            [thetas[j] * state.n_to_rds for j, state in enumerate(states)], axis=0
+        )
 
-if True:  # wrong models
-    # if there was constant coverage of the RDS state,
-    #   the current relative to j0 would simply be:
-    j_over_j0_simple = np.exp(alpha_0 * F * (u - U0) / (R * T0))
+        for j, state in enumerate(states):
+            ax2.plot(u, thetas[j], state.color)
+        ax3.plot(u, n_to_rds_vec, "r--")
 
-    # if there were two sites with diff. symmetry factors, we might get something like:
-    j_over_j0_early_site = 0.99 * np.exp(0.1 * F * (u - U0) / (R * T0))
-    j_over_j0_late_site = 0.01 * np.exp(0.9 * F * (u - U0) / (R * T0))
-    j_over_j0_2_simple = j_over_j0_early_site + j_over_j0_late_site
+        # ax2.set_xlabel("U$_{RHE}$ / [V]")
+        ax2.set_ylabel("coverage")
+        ax3.set_ylabel("<n> before RDS")
 
-    # lets plot the other two models:
-    ax1.plot(u, np.log10(j_over_j0_simple), "k--")
-    ax1.plot(u, np.log10(j_over_j0_2_simple * 1e-4), "k:")
+    if True:
+        # delta G plot
+        u_vec = np.array([1.23, 1.55])
+        for state in states:
+            n = state.n_to_rds
+            G0 = state.eV_1p23_vs_rds
+            G_vec = G0 + (u_vec - U0) * n
+            ax4.plot(u_vec, G_vec, state.color)
 
+        ax4.set_ylim(top=0.1)
 
-fig.set_figheight(fig.get_figwidth() * 1.5)
-fig.savefig("paper_I_v5_fig5.png")
-fig.savefig("paper_I_v5_fig5.svg")
+        ax4.set_xlabel("U vs RHE / [V]")
+        ax4.set_ylabel("$G^0 - G^0_{RDS}$ / [eV]")
+
+    if True:  # wrong models
+        # if there was constant coverage of the RDS state,
+        #   the current relative to j0 would simply be:
+        j_over_j0_simple = np.exp(alpha_0 * F * (u - U0) / (R * T0))
+
+        # if there were two sites with diff. symmetry factors, we might get something like:
+        j_over_j0_early_site = 0.99 * np.exp(0.1 * F * (u - U0) / (R * T0))
+        j_over_j0_late_site = 0.01 * np.exp(0.9 * F * (u - U0) / (R * T0))
+        j_over_j0_2_simple = j_over_j0_early_site + j_over_j0_late_site
+
+        # lets plot the other two models:
+        ax1.plot(u, np.log10(j_over_j0_simple), "k--")
+        ax1.plot(u, np.log10(j_over_j0_2_simple * 1e-4), "k:")
+
+    fig.set_figheight(fig.get_figwidth() * 1.5)
+    fig.savefig("paper_I_v5_fig5.png")
+    fig.savefig("paper_I_v5_fig5.svg")
